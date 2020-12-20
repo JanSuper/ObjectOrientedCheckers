dea4f44ce4e1c2fc32778bf22fcdfa8c12ad6562
Jan Super <Jan Super>
2020-12-20 13:01:06 +0100

    correct alphabeta pruning improved minimax tree from depth 5 to depth 7

--- ObjectOrientedCheckers/src/TrueMinimax/MinimaxNode.java
+++ ObjectOrientedCheckers/src/TrueMinimax/MinimaxNode.java
@@ -10,8 +10,6 @@ public class MinimaxNode {
 	public boolean pruned = false;
 	public boolean maxNode = true;
 	public int minimaxValue;
-	public int alpha = Integer.MIN_VALUE;
-	public int beta = Integer.MAX_VALUE;
 	
 	public Move madeMove = null;
 	public Object[][] beforeBoard;
@@ -27,13 +25,11 @@ public class MinimaxNode {
 		this.minimaxValue = Integer.MIN_VALUE;
 	}
 	
-	public MinimaxNode(Object[][] beforeBoard, Object[][] projectedboard, Move move, MinimaxNode parent, int alpha, int beta, boolean maxNode) {
+	public MinimaxNode(Object[][] beforeBoard, Object[][] projectedboard, Move move, MinimaxNode parent, boolean maxNode) {
 		this.beforeBoard = beforeBoard;
 		this.projectedBoard = projectedboard;
 		this.madeMove = move;
 		this.parent = parent;
-		this.alpha = alpha;
-		this.beta = beta;
 		this.maxNode = maxNode;
 		
 		if(this.maxNode) {
@@ -67,17 +63,17 @@ public class MinimaxNode {
 		}
 	}
 	
-	public void AlphaBeta() {
-		if(this.maxNode) {
-			this.alpha = Math.max(this.alpha, this.children.get(this.children.size() - 1).alpha);
-			this.alpha = Math.max(this.alpha, this.children.get(this.children.size() - 1).beta);
-		}
-		else {
-			this.beta = Math.min(this.beta, this.children.get(this.children.size() - 1).alpha);
-			this.beta = Math.min(this.beta, this.children.get(this.children.size() - 1).beta);
-		}
-		
-		this.pruned = this.alpha >= this.beta;
-	}
+//	public void AlphaBeta() {
+//		if(this.maxNode) {
+//			this.alpha = Math.max(this.alpha, this.children.get(this.children.size() - 1).alpha);
+//			this.alpha = Math.max(this.alpha, this.children.get(this.children.size() - 1).beta);
+//		}
+//		else {
+//			this.beta = Math.min(this.beta, this.children.get(this.children.size() - 1).alpha);
+//			this.beta = Math.min(this.beta, this.children.get(this.children.size() - 1).beta);
+//		}
+//		
+//		this.pruned = this.alpha >= this.beta;
+//	}
 
 }
--- ObjectOrientedCheckers/src/TrueMinimax/TrueMinimaxTree.java
+++ ObjectOrientedCheckers/src/TrueMinimax/TrueMinimaxTree.java
@@ -11,11 +11,15 @@ import Piece.Piece;
 
 public class TrueMinimaxTree {
 	
-	public static final int MAX_DEPTH = 5;
+	public static final int MAX_DEPTH = 7;
+	
+	public static int[][] alphabeta = new int[MAX_DEPTH][2];
 	
 	public static MinimaxNode rootNode;
 	
 	public static Move getMove(Object[][] board) {
+		
+		resetAlphaBeta();
 		
 		rootNode = new MinimaxNode(board);
 		
@@ -35,6 +39,13 @@ public class TrueMinimaxTree {
 		return null;
 	}
 	
+	public static void resetAlphaBeta() {
+		for(int i = 0; i <= MAX_DEPTH - 1; i++) {
+			alphabeta[i][0] = Integer.MIN_VALUE;
+			alphabeta[i][1] = Integer.MAX_VALUE;
+		}
+	}
+	
 	public static void recursion(MinimaxNode parentnode, int depth) {
 		
 		int amountOfEnemies = 0;		
@@ -52,24 +63,22 @@ public class TrueMinimaxTree {
 					
 					List<Move> holdList = ((Piece)holdBoard[i][j]).getMoves(); // Hold onto list of moves for simplicity
 					for(int k = 0; k <= holdList.size() - 1; k++) {// for every possible move	
-						if(!parentnode.pruned) {
-//							System.out.println("not pruned");
+						if(alphabeta[depth-1][0] < alphabeta[depth-1][1]) {
 							Object[][] tempBoard = Gamecontroller.deepBoardCopy(holdBoard);
 							int[] moveStats = AIController.moveScore(Gamecontroller.deepBoardCopy(holdBoard), MoveCalcTree.copyMove(holdList.get(k)));
 							Object[][] projectedBoard = AIMoveToAction.AIAction(MoveCalcTree.copyMove(holdList.get(k)), Gamecontroller.deepBoardCopy(tempBoard));
-							
-							int alpha = parentnode.alpha;
-							int beta = parentnode.beta;
+
 							boolean maxNode = !parentnode.maxNode;
-							MinimaxNode childNode = new MinimaxNode(tempBoard, projectedBoard, holdList.get(k), parentnode, alpha, beta, maxNode);
+							MinimaxNode childNode = new MinimaxNode(tempBoard, projectedBoard, holdList.get(k), parentnode, maxNode);
 							parentnode.children.add(childNode);
 							if(depth == MAX_DEPTH) {
-								parentnode.children.get(parentnode.children.size()-1).alpha = moveStats[0] + 2*moveStats[1] - (moveStats[2] + 2*moveStats[3]);
-								parentnode.children.get(parentnode.children.size()-1).beta = moveStats[0] + 2*moveStats[1] - (moveStats[2] + 2*moveStats[3]);
-								
+								if(parentnode.maxNode) {
+									alphabeta[depth-1][0] = Math.max(alphabeta[depth-1][0], moveStats[0] + 2*moveStats[1] - (moveStats[2] + 2*moveStats[3]));
+								}
+								else {
+									alphabeta[depth-1][1] = Math.min(alphabeta[depth-1][1], moveStats[0] + 2*moveStats[1] - (moveStats[2] + 2*moveStats[3]));
+								}
 								parentnode.scoreList.add(moveStats[0] + 2*moveStats[1] - (moveStats[2] + 2*moveStats[3]));
-								
-								
 							}
 							else {
 								if(parentnode.children.get(parentnode.children.size()-1).madeMove.getPiece().getColour() == 1) { // If White just made a move
@@ -78,12 +87,15 @@ public class TrueMinimaxTree {
 								else { // If Black just made a move
 									parentnode.children.get(parentnode.children.size()-1).projectedBoard = AIController.calcWhiteMoves(Gamecontroller.deepBoardCopy(parentnode.children.get(parentnode.children.size()-1).projectedBoard)); 
 								}
-							
+								
+								alphabeta[depth][0] = alphabeta[depth-1][0];
+								alphabeta[depth][1] = alphabeta[depth-1][1];
+								
 								recursion(parentnode.children.get(parentnode.children.size()-1), depth + 1);
 								}
 							}
 							else {
-//							System.out.println("pruned");
+//							System.out.println("pruned at depth " + depth);
 							}
 						}
 					}
@@ -94,16 +106,19 @@ public class TrueMinimaxTree {
 			if(parentnode.maxNode) {
 				parentnode.parent.scoreList.add(Integer.MIN_VALUE);
 				parentnode.minimaxValue = Integer.MIN_VALUE;
+				alphabeta[depth-2][1] = Math.min(alphabeta[depth-2][1], Integer.MIN_VALUE);
 			}
 			else {
 
 				if(amountOfEnemies == 0) {
 					parentnode.parent.scoreList.add(Integer.MAX_VALUE);
 					parentnode.minimaxValue = Integer.MAX_VALUE;
+					alphabeta[depth-2][0] = Math.max(alphabeta[depth-2][0], Integer.MAX_VALUE);
 				}
 				else {
 					parentnode.parent.scoreList.add(Integer.MIN_VALUE);
 					parentnode.minimaxValue = Integer.MIN_VALUE;
+					alphabeta[depth-2][0] = Math.max(alphabeta[depth-2][0], Integer.MIN_VALUE);
 				}
 			}
 			
@@ -112,11 +127,17 @@ public class TrueMinimaxTree {
 		}
 		else {
 			parentnode.Minimax();
-			
-			if(parentnode.parent != null) {
-//				parentnode.parent.AlphaBeta();
-			}
 		}
 		
+		if(parentnode.parent != null) {
+			if(parentnode.maxNode) {
+				alphabeta[depth-2][1] = Math.min(alphabeta[depth-2][1], alphabeta[depth-1][0]);
+				alphabeta[depth-2][1] = Math.min(alphabeta[depth-2][1], alphabeta[depth-1][1]);
+			}
+			else {
+				alphabeta[depth-2][0] = Math.max(alphabeta[depth-2][0], alphabeta[depth-1][0]);
+				alphabeta[depth-2][0] = Math.max(alphabeta[depth-2][0], alphabeta[depth-1][1]);
+			}
+		}
 	}
 }
