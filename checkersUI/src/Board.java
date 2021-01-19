ee3247a02570f84374bcd766b9867c46ef384c55
Jan Super <jansuper2001@gmail.com>
2021-01-19 17:30:54 +0100

    Revert "Revert "Add files via upload""
    This reverts commit b5408f1a54d8578a6a78136c51a42b781157f0cf.

---
+++ checkersUI/src/Board.java
@@ -0,0 +1,176 @@
+import javafx.scene.Group;
+
+import java.util.ArrayList;
+
+public class Board extends SmartGroup
+{
+
+    private static final double WIDTH_OF_TILE = 100;
+    private int[][] board;
+    public final ArrayList<RedPiece> redPieces = new ArrayList<RedPiece>();
+    public final ArrayList<BlackPiece> blackPieces = new ArrayList<BlackPiece>();
+    public final ArrayList<FakePiece> fakePieces = new ArrayList<FakePiece>();
+    public int turn = 0;
+
+    public Board()
+    {
+        initialise_board();
+        create_tiles();
+        place_pieces();
+        translateZProperty().set(getTranslateZ()+500);
+    }
+/**********************************************************************************************************************/
+    private void initialise_board()
+    {
+        board = new int[8][8];
+
+        for(int i = 0; i < 8; i=i+2)
+        {
+            board[0][i+1] = 1;
+            board[1][i] = 1;
+            board[2][i+1] = 1;
+            board[5][i] = 2;
+            board[6][i+1] = 2;
+            board[7][i] = 2;
+        }
+
+    }
+/**********************************************************************************************************************/
+    private void create_tiles()
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            for(int i = 0; i < 4; i++)
+            {
+                WhiteTile wt1 = new WhiteTile();
+                wt1.translateXProperty().set(wt1.getTranslateX()+WIDTH_OF_TILE*i*2);
+                wt1.translateYProperty().set(wt1.getTranslateY()+WIDTH_OF_TILE*j*2);
+
+                WhiteTile wt2 = new WhiteTile();
+                wt2.translateXProperty().set(wt2.getTranslateX()+WIDTH_OF_TILE*(i*2+1));
+                wt2.translateYProperty().set(wt2.getTranslateY()+WIDTH_OF_TILE*(j*2+1));
+
+
+                BlackTile bt1 = new BlackTile();
+                bt1.translateXProperty().set(bt1.getTranslateX()+WIDTH_OF_TILE*(i*2+1));
+                bt1.translateYProperty().set(bt1.getTranslateY()+WIDTH_OF_TILE*j*2);
+
+                BlackTile bt2 = new BlackTile();
+                bt2.translateXProperty().set(bt2.getTranslateX()+WIDTH_OF_TILE*i*2);
+                bt2.translateYProperty().set(bt2.getTranslateY()+WIDTH_OF_TILE*(j*2+1));
+
+                getChildren().addAll(bt1, wt1, bt2, wt2);
+            }
+        }
+    }
+
+/**********************************************************************************************************************/
+
+    private void place_pieces()
+    {
+
+        for(int i = 0; i < 8; i++)
+        {
+            for(int j = 0; j < 8; j++)
+            {
+                if(board[i][j] == 1)
+                {
+                    BlackPiece bp = new BlackPiece(this);
+                    bp.translateXProperty().set(bp.getTranslateX()+100*(j-1));
+                    bp.translateYProperty().set(bp.getTranslateY()+100*i);
+                    getChildren().add(bp);
+                }
+
+                if(board[i][j] == 2)
+                {
+                    RedPiece rp = new RedPiece(this);
+                    rp.translateXProperty().set(rp.getTranslateX()+100*(j-1));
+                    rp.translateYProperty().set(rp.getTranslateY()+100*i);
+                    getChildren().add(rp);
+                }
+            }
+        }
+    }
+
+/**********************************************************************************************************************/
+    public boolean piece_exists(double x, double y)
+    {
+        for(RedPiece rp : redPieces)
+        {
+            if( rp.getXCoord() == x && rp.getYCoord() == y)
+                return true;
+        }
+        for(BlackPiece bp : blackPieces)
+        {
+            if(bp.getXCoord() == x && bp.getYCoord() == y)
+                return true;
+        }
+
+        return false;
+    }
+
+/**********************************************************************************************************************/
+    public boolean occupied_by_enemy(double x, double y, int piece)
+    {
+        //if piece is black
+        if(piece == 0)
+        {
+            for(RedPiece rp : redPieces)
+            {
+                if( rp.getXCoord() == x && rp.getYCoord() == y)
+                    return true;
+            }
+        }
+        //if piece is red
+        if(piece == 1)
+        {
+            for(BlackPiece bp : blackPieces)
+            {
+                if(bp.getXCoord() == x && bp.getYCoord() == y)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+/*******************************************************************************/
+    public BlackPiece getBlackPiece(double x, double y)
+    {
+        for(BlackPiece bp : blackPieces)
+        {
+            if(bp.getXCoord() == x && bp.getYCoord() == y)
+                return bp;
+        }
+        return null;
+    }
+    public RedPiece getRedPiece(double x, double y)
+    {
+        for(RedPiece rp : redPieces)
+        {
+            if( rp.getXCoord() == x && rp.getYCoord() == y)
+                return rp;
+        }
+        return null;
+    }
+/*******************************************************************************/
+    public void endTurn() {
+        this.turn++;
+        //scan for end of game
+        if (this.blackPieces.size() == 0) {
+            System.out.println("Red wins");
+        }
+        if (this.redPieces.size() == 0){
+            System.out.println("Black wins");
+        }
+
+        //scan for kings
+        for(RedPiece rp: this.redPieces){
+            if(rp.getYCoord()==0 && !rp.king)
+                rp.king();
+        }
+        for(BlackPiece bp: this.blackPieces){
+            if(bp.getYCoord()==7 && !bp.king)
+                bp.king();
+        }
+    }
+}
---
+++ checkersUI/src/RedPiece.java
@@ -0,0 +1,214 @@
+import javafx.geometry.HPos;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+
+import java.util.ArrayList;
+
+public class RedPiece extends Piece
+{
+    private Board board;
+    public boolean king = false;
+
+    public RedPiece(Board board)
+    {
+        super();
+        this.board = board;
+        this.board.redPieces.add(this);
+        PhongMaterial material = new PhongMaterial();
+        material.setDiffuseColor(Color.DARKRED);
+        setMaterial(material);
+        addEvent();
+    }
+
+    public void king()
+    {
+        this.king = true;
+        PhongMaterial mt = new PhongMaterial();
+        mt.setDiffuseMap(new Image(String.valueOf(getClass().getResource("/images/RedCrown.png"))));
+        setMaterial(mt);
+        this.setOnMouseClicked(e->{
+            //if it's red's turn
+            if(this.board.turn%2==1)
+            {
+                //delete all fake pieces
+                for( FakePiece fp : this.board.fakePieces)
+                    this.board.getChildren().remove(fp);
+                this.board.fakePieces.clear();
+                kingMovements();
+            }
+        });
+    }
+
+    private boolean occupiedByEnemy(double x, double y)
+    {
+        return (this.board.occupied_by_enemy(x, y, 1));
+    }
+
+    private boolean isFree(double x, double y) {return (!this.board.piece_exists(x, y));}
+
+    private boolean isInBounds(double x, double y){
+        return x >= 0 && x <= 7 && y >= 0 && y <= 7;
+    }
+
+    private void addEvent()
+    {
+        this.setOnMouseClicked(e->{
+            //if it's red's turn
+            if(this.board.turn%2==1)
+            {
+
+                //delete all fake pieces
+                for( FakePiece fp : this.board.fakePieces)
+                    this.board.getChildren().remove(fp);
+                this.board.fakePieces.clear();
+                // first move
+                firstMove();
+            }
+        });
+    }
+
+
+    private void firstMove()
+    {
+        // current coordinates
+        double xCoord = this.getXCoord();
+        double yCoord = this.getYCoord();
+
+        // simple moves coordinates
+        double[] FrontX = {xCoord + 1, xCoord - 1};
+        double yFront = yCoord - 1;
+
+        // for the possible simple moves
+        for(double xFront : FrontX)
+        {
+            //check if coords are in bounds and free
+            if( isInBounds(xFront, yFront) && isFree(xFront, yFront) )
+            {
+                //create fake piece
+                FakePiece fp = new FakePiece(xFront, yFront, this, this.board);
+                //no eaten pieces
+            }
+            // else if coords are occupied by enemy
+            else if ( isInBounds(xFront, yFront) && occupiedByEnemy(xFront, yFront))
+            {
+                //landing space
+                double landX;
+                if(xFront == xCoord+1)
+                    landX = xFront+1;
+                else
+                    landX = xFront-1;
+                double landY = yFront - 1;
+
+                //if landing space is in bounds and free
+                if( isInBounds(landX, landY) && isFree(landX, landY))
+                {
+                    // create fake and add the piece to eat in the list
+                    FakePiece fp = new FakePiece(landX, landY, this, this.board);
+                    fp.eaten_pieces.add(this.board.getBlackPiece(xFront, yFront));
+                    // launch recursive function
+                    recurse(landX, landY, xFront, yFront, fp);
+                }
+            }
+        }
+    }
+    private void recurse(double X, double Y, double prevX, double prevY, FakePiece fp)
+    {
+        double[] xPoss = {X - 1, X + 1};
+        double[] yPoss = {Y - 1, Y + 1};
+
+        //for all positions around piece
+        for(double xCoord: xPoss)
+        {
+            for(double yCoord: yPoss)
+            {
+                //we don't check if we can eat back where we came from
+                if(!(xCoord == prevX && yCoord == prevY))
+                {
+                    //check if is in bounds and occupied by enemy
+                    if(isInBounds(xCoord, yCoord) && occupiedByEnemy(xCoord, yCoord))
+                    {
+                        //check landing space
+                        double landX, landY;
+                        if(xCoord == X-1)
+                            landX = X-2;
+                        else
+                            landX = X+2;
+                        if(yCoord == Y-1)
+                            landY = Y-2;
+                        else
+                            landY = Y+2;
+
+                        //if landing space is in bounds and is either free or occupied by OG piece
+                        if(isInBounds(landX, landY) && (isFree(landX, landY) ))
+                        {
+                            //create fake piece and recurse
+                            FakePiece f = new FakePiece(landX, landY, this, this.board);
+                            f.eaten_pieces.addAll(fp.eaten_pieces);
+                            f.eaten_pieces.add(this.board.getBlackPiece(xCoord, yCoord));
+                            recurse(landX, landY, xCoord, yCoord, f);
+                        }
+                        //else if we going back to OG position (counter-loop)
+                        else if (isInBounds(landX, landY) && (landX == this.getXCoord() && landY == this.getYCoord()))
+                        {
+                            FakePiece f = new FakePiece(landX, landY, this, this.board);
+                            f.eaten_pieces.addAll(fp.eaten_pieces);
+                            f.eaten_pieces.add(this.board.getBlackPiece(xCoord, yCoord));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+/**********************************************************************************************************************/
+    private void kingMovements()
+    {
+        // current coordinates
+        double xCoord = this.getXCoord();
+        double yCoord = this.getYCoord();
+
+        // simple moves coordinates
+        double[] FrontX = {xCoord + 1, xCoord - 1};
+        double[] FrontY = {yCoord - 1, yCoord + 1};
+
+        // for the possible simple moves
+        for(double xFront : FrontX)
+        {
+            for(double yFront: FrontY)
+            {
+                {
+                    //check if coords are in bounds and free
+                    if( isInBounds(xFront, yFront) && isFree(xFront, yFront) )
+                    {
+                        //create fake piece
+                        FakePiece fp = new FakePiece(xFront, yFront, this, this.board);
+                        //no eaten pieces
+                    }
+                    // else if coords are occupied by enemy
+                    else if ( isInBounds(xFront, yFront) && occupiedByEnemy(xFront, yFront))
+                    {
+                        //landing space
+                        double landX;
+                        if(xFront == xCoord+1)
+                            landX = xFront+1;
+                        else
+                            landX = xFront-1;
+                        double landY = yFront - 1;
+
+                        //if landing space is in bounds and free
+                        if( isInBounds(landX, landY) && isFree(landX, landY))
+                        {
+                            // create fake and add the piece to eat in the list
+                            FakePiece fp = new FakePiece(landX, landY, this, this.board);
+                            fp.eaten_pieces.add(this.board.getBlackPiece(xFront, yFront));
+
+                            // launch recursive function
+                            recurse(landX, landY, xFront, yFront, fp);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
